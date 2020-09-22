bb0db539bbf2f3f8f0dfc24e9ff740f71c0449de
kim66003 <kim66003@gmail.com>
2020-09-22 16:40:33 +0200

    indiv gain results

--- show_solution.py
+++ show_solution.py
@@ -13,6 +13,7 @@ import sys, os
 sys.path.insert(0, 'evoman') 
 from environment import Environment
 from demo_controller import player_controller
+import numpy as np
 
 os.makedirs("results/", exist_ok=True)
 experiment_name = "results/EA_1/"
@@ -24,10 +25,30 @@ n_hidden_neurons = 10
 # initializes environment with ai player using random controller, playing against static enemy
 env = Environment(experiment_name=experiment_name,
                   player_controller=player_controller(n_hidden_neurons),
-                  enemy_mode="static",
+                  enemymode="static",
                   level=2
                   )
 
 # TODO Load solution
-# np.loadtxt?
-env.play()
+mutations = ['uniform','normal']
+
+for mutation in mutations:
+	# tests saved demo solutions for each enemy
+    for enemy in range (1,4):
+        gains = []
+        for train in range(0, 10):
+            mean_indv_gain = []
+            for run in range(5):
+                print('\n LOADING SAVED SOLUTION FOR ENEMY: '+str(enemy)+' mutation: '+str(mutation) + ' training: '+str(train))
+                # Update the enemy  
+                env.update_parameter('enemies',[enemy])
+                # Load specialist controller
+                solution = np.loadtxt('results/task1/best_enemy'+str(enemy)+'_train'+str(train)+'_mut'+mutation+'.txt')
+                env.play(solution)
+                individual_gain = env.get_playerlife() - env.get_enemylife()
+                mean_indv_gain.append(individual_gain)
+                print('individual gain', env.get_playerlife() - env.get_enemylife())
+            gains.append(np.mean(mean_indv_gain))
+
+        # write individual gain to text file
+        np.savetxt('results/best_solutions/individual_gain_enemy{}_mut{}'.format(enemy, mutation), gains)
