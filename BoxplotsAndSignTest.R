085c685a4d9191ceef150030cb75681d8edba359
Timo <cobravspanda01@gmail.com>
2020-09-25 12:24:23 +0200

    wilcox.test

--- BoxplotsAndSignTest.R
+++ BoxplotsAndSignTest.R
@@ -2,9 +2,9 @@ library(ggplot2)
 
 loadFile <- function(fileName, enemy, mutation){
   df <-data.frame(read.table(fileName))
-  colnames(df) <- c('Individual Gain')
+  colnames(df) <- c('IndividualGain')
   df$enemy <- enemy
-  df$mutation <- mutation 
+  df$mutation <- as.factor(mutation)
   return(df)
 }
 
@@ -21,5 +21,15 @@ enem3norm <- loadFile("individual_gain_enemy3_mutnormal",3,'normal')
 combined <- rbind(rbind(rbind(rbind(rbind(enem1norm,enem1uniform),enem3norm),enem3uniform),enem2norm),enem2uniform)
 combined$enemy <- as.factor(combined$enemy)
 combined$mutation<- as.factor(combined$mutation)
-ggplot(combined,aes(x =enemy, y = `Individual Gain`,fill =mutation)) + geom_boxplot()
+ggplot(combined,aes(x =enemy, y = IndividualGain,fill =mutation)) + geom_boxplot()
 
+#Wilcox test
+combinedEnemy1 = rbind(enem1uniform,enem1norm)
+combinedEnemy2 <- rbind(enem2uniform,enem2norm)
+combinedEnemy3 <- rbind(enem3norm,enem3uniform)
+res1 <- wilcox.test(IndividualGain ~ mutation, data = combinedEnemy1, exact = FALSE)
+res1
+res2 <- wilcox.test(IndividualGain ~ mutation, data = combinedEnemy2, exact = FALSE)
+res2
+res3 <- wilcox.test(IndividualGain ~ mutation, data = combinedEnemy3, exact = FALSE)
+res3
